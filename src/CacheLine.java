510e0037c27aae5bb823b128b43044d4b2cf0231
Chris Sun <chris438@gmail.com>
2016-06-06 15:07:10 -0700

    Add Mesi State to cache line

--- src/CacheLine.java
+++ src/CacheLine.java
@@ -1,21 +1,36 @@
 
 public class CacheLine {
+	public enum MESIState {
+		MODIFIED, EXCLUSIVE, SHARED, INVALID;
+	}
+	
 	private int tag;
+	
 	private boolean isValid;
+	
+	private MESIState mesiState;
 	
 	public CacheLine() {
 		this.tag = 0;
 		this.isValid = false;
 	}
+	
+	public void setMesiState(MESIState mesiState) {
+		this.mesiState = mesiState;
+	}
+	
 	public void setTag(int tag) {
 		this.tag = tag;
 	}
+	
 	public int getTag() { 
 		return tag;
 	}
+	
 	public void setIsValid(boolean isValid){
 		this.isValid =isValid;
 	}
+	
 	public boolean isValid() {
 		return isValid;
 	}
